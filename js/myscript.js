afe42ef3807abebbd256669eb8036d87bcc59b45
Vale <oirelav95@gmail.com>
2022-11-12 16:55:27 +0100

    aggiunto funzionamento bottoni prev e next

--- js/myscript.js
+++ js/myscript.js
@@ -6,6 +6,10 @@ for (let i = 1; i <= 5; i++) {
 
     const slider = document.querySelector(".slider");
 
+    const next = document.querySelector(".next");
+
+    const prev = document.querySelector(".prev");
+
     let activeItem = 0;
 
     slider.innerHTML += `
@@ -16,6 +20,51 @@ for (let i = 1; i <= 5; i++) {
     const imageArray = document.getElementsByClassName("immagine");
 
     imageArray[activeItem].classList.add("active");
+
+    next.addEventListener("click", 
+        function() {
+
+            if (activeItem < imageArray.length - 1) {
+                imageArray[activeItem].classList.remove("active");
+
+                activeItem++;
+
+                imageArray[activeItem].classList.add("active");
+
+            }
+
+            else {
+                activeItem = 0;
+
+                imageArray[activeItem].classList.add("active");
+                imageArray[imageArray.length - 1].classList.remove("active");
+            }
+
+        }
+    );                
+
+    prev.addEventListener("click", 
+        function() {
+
+            if (activeItem <= imageArray.length - 1 && activeItem > 0) {
+                imageArray[activeItem].classList.remove("active");
+
+                activeItem--;
+
+                imageArray[activeItem].classList.add("active");
+
+            }
+
+            else {
+                activeItem = imageArray.length - 1;
+
+                imageArray[activeItem].classList.add("active");
+                
+                imageArray[0].classList.remove("active");
+            }
+
+        }
+    );
 
     console.log(imageArray);
 }
